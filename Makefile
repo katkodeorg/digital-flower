32b65be6cd54bb3bdbb498eab5186d682869579e
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2024-10-19 16:26:50 +0530

    Update

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
 .PHONY: web
 web:
 	npm run build
-	rm -rf ./docs
-	mv ./out ./docs
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // <=== enables static exports
+  output: "export",
+  distDir: "docs",
 };
 
 export default nextConfig;
